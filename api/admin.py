a5e6f324c3e45eedb05dd90f6afc541ead477e34
Eeshu Yadav <153345870+Eeshu-Yadav@users.noreply.github.com>
2024-06-27 18:13:00 +0530

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,25 +1,22 @@
 from django.contrib import admin
-from .models import Portfolio, LegSettings, LegExecutionDetails
+from .models import Portfolio, LegSettings, LegExecutionDetails, RequestData
 
 @admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
-    list_display = ('name', 'expiry', 'premium_gap', 'start', 'end', 'target', 'stop_loss')
-    search_fields = ('name',)
-    list_filter = ('expiry',)
+    list_display = ('name', 'expiry', 'start', 'end', 'target', 'stop_loss')
+    search_fields = ('name', 'expiry')
 
 @admin.register(LegSettings)
 class LegSettingsAdmin(admin.ModelAdmin):
-    list_display = (
-        'portfolio', 'state', 'right', 'txn', 'execution_time', 'sqoff_time', 
-        'count_sl', 'count_tp', 'wait_n_trade', 'target_premium', 'stop_loss', 
-        'take_profit', 'ProfitLockThreshold', 'LockProfitAt', 
-        'IncreaseInProfitForTrail', 'TrailProfitBy', 'SL_TrailTrigger', 
-        'SL_Trail_Amt', 'on_target', 'on_tp', 'on_sl'
-    )
-    search_fields = ('portfolio__name',)
-    list_filter = ('state', 'right', 'txn', 'on_target', 'on_tp', 'on_sl')
+    list_display = ('portfolio', 'state', 'right', 'txn', 'execution_time', 'sqoff_time', 'count_sl', 'count_tp', 'target_premium', 'stop_loss', 'take_profit', 'on_target', 'on_tp', 'on_sl')
+    search_fields = ('portfolio__name', 'right', 'txn')
 
 @admin.register(LegExecutionDetails)
 class LegExecutionDetailsAdmin(admin.ModelAdmin):
     list_display = ('sqoff', 'execute', 'rexecute', 'rentry')
     search_fields = ('sqoff', 'execute', 'rexecute', 'rentry')
+
+@admin.register(RequestData)
+class RequestDataAdmin(admin.ModelAdmin):
+    list_display = ('endpoint', 'timestamp')
+    search_fields = ('endpoint', 'timestamp')
